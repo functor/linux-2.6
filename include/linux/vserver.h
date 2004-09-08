5cca0e7c9dc74a05b474f9c85b18b5964ba1de35
Planet-Lab Support <support@planet-lab.org>
2004-09-08 17:46:52 +0000

    This commit was manufactured by cvs2svn to create tag 'after-ipod-patch'.

--- include/linux/vserver.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LINUX_VSERVER_H
-#define _LINUX_VSERVER_H
-
-#include <linux/vserver/context.h>
-#include <linux/vserver/network.h>
-#include <linux/vinline.h>
-#include <linux/ninline.h>
-
-#endif
