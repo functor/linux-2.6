85a253e419efd07f307ff5057eca9f8468af9670
Planet-Lab Support <support@planet-lab.org>
2005-01-11 04:56:08 +0000

    This commit was manufactured by cvs2svn to create tag 'before-shares_write_debug-merge'.

--- include/asm-ia64/netdump.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ASM_IA64_NETDUMP_H_
-#define _ASM_IA64_NETDUMP_H_
-
-#include <asm-generic/netdump.h>
-
-#endif /* _ASM_IA64_NETDUMP_H_ */
--- include/linux/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LINUX_CONFIG_H
-#define _LINUX_CONFIG_H
-
-#include <linux/autoconf.h>
-#ifndef __KERNEL__
-#error including kernel header in userspace; use the glibc headers instead!
-#endif
-#endif
