ba1261816a727110b5d72df85f6d3eccf9e06288
Mark Huang <mlhuang@cs.princeton.edu>
2005-08-20 22:17:20 +0000

    - sync vserver branch

--- arch/x86_64/ia32/sys_ia32.c
+++ arch/x86_64/ia32/sys_ia32.c
@@ -61,7 +61,6 @@
 #include <linux/ptrace.h>
 #include <linux/highuid.h>
 #include <linux/vmalloc.h>
-#include <linux/vs_cvirt.h>
 #include <asm/mman.h>
 #include <asm/types.h>
 #include <asm/uaccess.h>
