6d1f583df81cd36e2a0bd25025f5082de5802b67
Mark Huang <mlhuang@cs.princeton.edu>
2005-08-20 23:13:19 +0000

    - sync vserver branch

--- arch/ia64/kernel/perfmon.c
+++ arch/ia64/kernel/perfmon.c
@@ -38,8 +38,6 @@
 #include <linux/pagemap.h>
 #include <linux/mount.h>
 #include <linux/version.h>
-#include <linux/vs_memory.h>
-#include <linux/vs_cvirt.h>
 #include <linux/bitops.h>
 #include <linux/vs_memory.h>
 #include <linux/vs_cvirt.h>
