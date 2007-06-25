517e2d4ec73bfd813f187a3f94a6aa20ff0b6fa2
Planet-Lab Support <support@planet-lab.org>
2007-06-25 16:33:03 +0000

    This commit was manufactured by cvs2svn to create tag 'fedora-2_6_20-1_2949_fc6_vs2_2_0_1'.

--- include/linux/vserver.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LINUX_VSERVER_H
-#define _LINUX_VSERVER_H
-
-#include <linux/vserver/context.h>
-#include <linux/vserver/network.h>
-
-extern long vs_reboot(unsigned int, void *);
-
-#endif
