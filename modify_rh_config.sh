afb0d83312c40e95d4f705e45695bce4cc7307ba
S.Çağlar Onur <caglar@verivue.com>
2011-03-16 15:20:58 -0400

    less output

--- modify_rh_config.sh
+++ modify_rh_config.sh
@@ -12,4 +12,4 @@ if ! grep -q Scientific /etc/redhat-release; then
 	sed -i -e "s,# CONFIG_SYSFS_DEPRECATED_V2 is not set,CONFIG_SYSFS_DEPRECATED_V2=y,g" config-generic
 fi
 
-popd
+popd > /dev/null
