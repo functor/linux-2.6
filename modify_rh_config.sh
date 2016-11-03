f0b0620440dd239bc3822c0d8136054e2634581a
functor <mw>
2016-11-02 20:13:09 -0400

    Disabling the experimental in-kernel signature checker build option.

--- modify_rh_config.sh
+++ modify_rh_config.sh
@@ -6,6 +6,10 @@ sed -i -e "s,CONFIG_IPV6=m,CONFIG_IPV6=y,g" config-generic
 sed -i -e "/\# CONFIG_KVM is not set/d" config-x86-generic-rhel
 sed -i -e "s,CONFIG_MODULE_SIG=y,CONFIG_MODULE_SIG=n,g" config-generic-rhel
 
+sed -i -e "s,CONFIG_CRYPTO_SIGNATURE=y,CONFIG_CRYPTO_SIGNATURE=n,g" config-generic-rhel
+sed -i -e "s,CONFIG_CRYPTO_SIGNATURE_DSA=y,CONFIG_CRYPTO_SIGNATURE_DSA=n,g" config-generic-rhel
+
+
 # Do not enable CONFIG_SYSFS_DEPRECATED options for Scientific Linux
 if ! grep -qiE "CentOS.*6.3|Scientific" /etc/redhat-release; then
 	sed -i -e "s,# CONFIG_SYSFS_DEPRECATED is not set,CONFIG_SYSFS_DEPRECATED=y,g" config-generic
